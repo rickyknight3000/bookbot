4e3001b2e9dcf79daeabca4d67fb24c3f4e9d74d
rickyknight3000 <ricardovcapitao@hotmail.com>
2025-06-15 20:24:16 -0400

    fixed bugs with wrong input

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from stats import (
 )
 
 def main():
-    book_path = sys.argv[1]
     if len(sys.argv) < 2:
         print("Usage: python3 main.py <path_to_book>")
         sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_chars = get_num_characters(text)
